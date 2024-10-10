60be4d8f4eeded2763b1a8a1c2e405bfd0425eb5
lukasnf <lukasfilipon7@gmail.com>
2024-10-10 21:31:38 +0200

    *updated collision_angle* still working on it

--- Compton Effekt.py
+++ Compton Effekt.py
@@ -12,7 +12,7 @@ c = 3e8
 m_elektron = 9.11e-31
 h = 6.626e-34
 f = 3e19
-theta = 180 * ((2*np.pi)/360)
+theta = 180 * ((2*np.pi)/360) #change angle if needed
 
 
 class Electron:
@@ -55,7 +55,7 @@ class Photon:
     def draw(self):
         self.circle = pygame.draw.circle(screen,self.color,(self.pos_x,self.pos_y),self.radius)
     def scattering(self,electron):
-        if check_collision(self,electron) == True:
+        if check_collision(self, electron):
             self.v_y = self.velocity * np.sin(theta)
             self.v_x = self.velocity * np.cos(theta)
             self.pos_x += self.v_x
@@ -77,12 +77,23 @@ def text(text,font,color,x,y):
 def compton_scattering(photon):
     lambda_0 = photon.wavelength
     delta_lambda = (h/(m_elektron*c)*(1-np.cos(theta)))
-    new_wavelenght = lambda_0 + delta_lambda
-    photon.wavelength = new_wavelenght
+    new_wavelength = lambda_0 + delta_lambda
+    photon.wavelength = new_wavelength
+
+def collision_angle(photon,electron):
+    delta_x = electron.pos_x - photon.pos_x
+    delta_y = electron.pos_y - photon.pos_y
+    if delta_x == 0:
+        if delta_y > 0:
+            return 90 * (180/np.pi)
+        else: return -90 * (180/np.pi)
+    angle = np.arctan2(delta_y,delta_x)
+    print(f"delta_x: {delta_x}, delta_y: {delta_y}")
+    return angle * (180/np.pi)
 
 
 
-photon = Photon(200,600,10,10,np.sqrt(10**2 + 10**2),"white",15,c/f) #change velocity if needed
+photon = Photon(860,600,10,10,np.sqrt(10**2 + 10**2),"white",15,c/f) #change velocity if needed
 electron = Electron(860,400,0,0,m_elektron,"yellow",20)
 collision = False
 
@@ -103,6 +114,9 @@ while True:
         compton_scattering(photon)
         collision = True
         photon.scattering(electron)
+        print(collision_angle(photon, electron))
+
+
 
     text(f"λ: {photon.wavelength} m",font,"white",1100,90)
     text(f"p: {(h*f)/photon.velocity} Ns", font, "white", 1100, 110)
